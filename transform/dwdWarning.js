1c3e40b2d11c3347c56a06e86ac9343724740d43
Plitt Jonas <jplitt@alegri.eu>
2018-03-02 08:58:09 +0100

    extended validation with times

--- transform/dwdWarning.js
+++ transform/dwdWarning.js
@@ -3,14 +3,14 @@
 	//var regex = /(?:{\"regionName\":\"Ennepe-Ruhr-Kreis\"[^}]+\"description\":\")([^}"]+)(?:\"[^}]+\"event\":\")([^}"]+)(?:\"[^}]+)(?:})/ig;
     //var res = lines.match(/{\"regionName\":\"Ennepe-Ruhr-Kreis\"[^}]+\"description\":\"([^}"])+\"[^}]+\"event\":\"([^}"])+\"[^}]+}/ig).toString();
 
-    var myRegexp = /(?:{\"regionName\":\"Ennepe-Ruhr-Kreis\"[^}]+\"description\":\")([^}"]+)(?:\"[^}]+\"event\":\")([^}"]+)(?:\"[^}]+)(?:})/ig;
+    var myRegexp = /(?:{\"regionName\":\"Ennepe-Ruhr-Kreis\")(?:[",]+)(?:\"start\":\")([\d\s.:]{16})(?:[",]+)(?:\"end\":\")([\d\s.:]{16})(?:[^}]+)(?:\"description\":\")([^}"]+)(?:\"[^}]+\"event\":\")([^}"]+)(?:\"[^}]+)(?:})/ig;
     match = myRegexp.exec(lines);
     var res2 = '';
     while (match != null) {
       // matched text: match[0]
       // match start: match.index
       // capturing group n: match[n]
-      res2 += match[2] + ': ' + match[1] + '\r\n';
+      res2 += match[4] + ': ' + match[3] + ' (' + match[1] + ' - ' + match[2] + ')\r\n';
       match = myRegexp.exec(lines);
     }
 
